d1cc31d152d691fe5bb91381d24cec8db1d16abf
simonuks221 <simonasriauka@gmail.com>
2021-06-12 16:30:45 +0300

    Database code change

--- schedule-server/index.js
+++ schedule-server/index.js
@@ -5,10 +5,20 @@ const app = express()
 
 const workbook = XLSX.readFile('datasheet.xlsx')
 
-let worksheet = {}
+let worksheets = {}
 const getExelData = () => {
-    worksheet = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]])
-    return worksheet
+    //titleSheet = workbook.SheetNames.find(sheet => 'TitleSheet')
+    titleSheet = XLSX.utils.sheet_to_json(workbook.Sheets['TitleSheet'])
+    for(const sheetName of workbook.SheetNames){
+        if(sheetName !== 'TitleSheet'){
+            worksheet = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName])
+            foundSheet = titleSheet.find(a => a.Title === sheetName)
+            console.log(foundSheet)
+            foundSheet.Times = worksheet
+        }
+    }
+    //worksheet = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]])
+    return titleSheet
 }
 
 app.get('/api/timetable', (req, res) => {
