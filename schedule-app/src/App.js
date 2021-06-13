db8af5bb4ac7bc0de8eee331da598acd6bb387f0
simonuks221 <simonasriauka@gmail.com>
2021-06-13 16:29:30 +0300

    Removed backchange in mockup data

--- schedule-app/src/App.js
+++ schedule-app/src/App.js
@@ -32,7 +32,6 @@ function App() {
   const setMockupData = () => {
     const newTime = {ID: 1, Title: 'To Klaipėda', backgroundImage: './klaipeda.webp', Description: 'A ferry to Klaipėda from Smiltynė', Times: [{ID: 1, TimeHours: 6, TimeMinutes: 20}, {ID: 2, TimeHours: 7, TimeMinutes: '00'}, {ID: 3, TimeHours: 8, TimeMinutes: 20}, {ID: 4, TimeHours: 9, TimeMinutes: '00'}, {ID: 5, TimeHours: 10, TimeMinutes: 20}, {ID: 6, TimeHours: 11, TimeMinutes: '00'}, {ID: 7, TimeHours: 12, TimeMinutes: 20}, {ID: 8, TimeHours: 13, TimeMinutes: '00'}]}
     const newTime2 = {ID: 2, Title: 'To Smiltynė', backgroundImage: './smiltyne.jpg', Description: 'A ferry to Smiltynė from Klaipėda', Times: [{ID: 1, TimeHours: 6, TimeMinutes: 20}]}
-    setCurrentIndex(1)
     setTimetable(times => [newTime, newTime2])
   }
 
