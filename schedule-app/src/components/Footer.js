43220e53542a890160607c3d9e982189cf21c715
simonuks221 <simonasriauka@gmail.com>
2021-06-13 15:29:16 +0300

    Visual changes

--- schedule-app/src/components/Footer.js
+++ schedule-app/src/components/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
-        <footer className = 'border'>
-            <div className = 'text-center mt-3'>
-                <p className = 'text-center d-inline'> By: Simonas Riauka on <a className = 'd-inline' href = 'https://github.com/simonuks221'> GitHub</a></p>
+        <footer className = 'border mt-3'>
+            <div className = 'py-3'>
+                <p className = 'text-center d-inline pl-3'> By: Simonas Riauka on <a className = 'd-inline' href = 'https://github.com/simonuks221'> GitHub</a></p>
             </div>
         </footer>
     )
--- schedule-app/src/components/TimetableNav.js
+++ schedule-app/src/components/TimetableNav.js
@@ -2,11 +2,13 @@ import TimetableNavPart from './TimetableNavPart.js'
 
 const TimetableNav = ({timetable, changeTimetable, currentIndex}) => {
     return (
-        <nav aria-label = 'breadcrumb'>
-            <ol className = 'breadcrumb'>
-                {timetable.map((timeData) => <TimetableNavPart key = {timeData.ID} timeData = {timeData} changeTimetable = {changeTimetable} currentIndex = {currentIndex}/>)}
-            </ol>
-        </nav>
+        <div className = 'container-fluid timeNav'>
+            <nav aria-label = 'breadcrumb'>
+                <ol className = 'breadcrumb'>
+                    {timetable.map((timeData) => <TimetableNavPart key = {timeData.ID} timeData = {timeData} changeTimetable = {changeTimetable} currentIndex = {currentIndex}/>)}
+                </ol>
+            </nav>
+        </div>
     )
 }
 
