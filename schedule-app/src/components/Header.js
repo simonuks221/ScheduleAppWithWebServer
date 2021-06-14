14fa17736cfe959504874ab8f5e0f718f8488c1d
simonuks221 <simonasriauka@gmail.com>
2021-06-14 14:27:45 +0300

    Tried navbar expansion

--- schedule-app/src/components/Header.js
+++ schedule-app/src/components/Header.js
@@ -2,20 +2,22 @@ import {Link} from 'react-router-dom'
 
 const Header = () => {
     return (
-        <nav className = 'navbar navbar-expand-sm headerStyle mb-3'>
+        <nav className = 'navbar navbar-expand headerStyle mb-3'>
             <div className = 'container-fluid'>
                 <Link className = 'text-left navbar-brand text-dark logoText' to = '/'>Scheduler</Link>
-                <ul className = 'navbar-nav mb-2 text-left mr-auto mb-2 mb-sm-0'>
-                    <li>
-                        <Link className = 'button' to = '/'>Schedule</Link>
-                    </li>
-                    <li>
-                        <Link className = 'button' to = '/pricing' >Pricing</Link>
-                    </li>
-                    <li>
-                        <Link className = 'button' to = '/about'>About</Link>
-                    </li>
-                </ul>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className = 'navbar-nav mb-2 text-left mr-auto mb-2 mb-sm-0'>
+                        <li className = 'nav-item'>
+                            <Link className = 'button' to = '/'>Schedule</Link>
+                        </li>
+                        <li>
+                            <Link className = 'button' to = '/pricing' >Pricing</Link>
+                        </li>
+                        <li>
+                            <Link className = 'button' to = '/about'>About</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
